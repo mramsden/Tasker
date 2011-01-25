1fe2c48b4a24e4aa356604857d61a22a877b7314
Marcus Ramsden <cuscus1986@gmail.com>
2011-01-25 22:03:29 +0000

    Created the tasks data source.

--- src/com/example/tasker/Tasker.java
+++ src/com/example/tasker/Tasker.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class Tasker extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
