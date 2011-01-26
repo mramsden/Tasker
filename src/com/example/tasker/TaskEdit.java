8735c1daeb6100406013fa73dfa2ee949bf69b8a
Marcus Ramsden <cuscus1986@gmail.com>
2011-01-26 22:49:03 +0000

    The TaskEdit action now handles state and supports the back button correctly.

--- src/com/example/tasker/TaskEdit.java
+++ src/com/example/tasker/TaskEdit.java
@@ -1,7 +1,7 @@
 package com.example.tasker;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,11 +13,78 @@ public class TaskEdit extends Activity {
 	private EditText mBodyText;
 	private Long mRowId;
 	
+	private TasksDbAdapter mDbHelper;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		mDbHelper = new TasksDbAdapter(this);
+		mDbHelper.open();
+		
 		setContentView(R.layout.task_edit);
 		setTitle(R.string.edit_task);
+		
+		mTitleText = (EditText)findViewById(R.id.title);
+		mBodyText = (EditText)findViewById(R.id.body);
+		
+		Button confirmButton = (Button)findViewById(R.id.confirm);
+		
+		mRowId = (savedInstanceState == null) ? null : (Long)savedInstanceState.getSerializable(TasksDbAdapter.KEY_ROWID);
+		if (mRowId == null) {
+			Bundle extras = getIntent().getExtras();
+			mRowId = (extras != null) ? extras.getLong(TasksDbAdapter.KEY_ROWID) : null;
+		}
+		
+		populateFields();
+		
+		confirmButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				setResult(RESULT_OK);
+				finish();
+			}
+		});
 	}
 	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		saveState();
+		outState.putSerializable(TasksDbAdapter.KEY_ROWID, mRowId);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		saveState();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		populateFields();
+	}
+	
+	private void populateFields() {
+		if (mRowId != null) {
+			Cursor task = mDbHelper.fetchTask(mRowId);
+			startManagingCursor(task);
+			mTitleText.setText(task.getString(task.getColumnIndexOrThrow(TasksDbAdapter.KEY_TITLE)));
+			mBodyText.setText(task.getString(task.getColumnIndexOrThrow(TasksDbAdapter.KEY_BODY)));
+		}
+	}	
+	
+	private void saveState() {
+		String title = mTitleText.getText().toString();
+		String body = mBodyText.getText().toString();
+		
+		if (mRowId == null) {
+			long id = mDbHelper.createTask(title, body);
+			if (id > 0) {
+				mRowId = id;
+			}
+		} else {
+			mDbHelper.updateTask(mRowId, title, body);
+		}
+	}
 }
--- src/com/example/tasker/Tasker.java
+++ src/com/example/tasker/Tasker.java
@@ -1,16 +1,25 @@
 package com.example.tasker;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class Tasker extends ListActivity {
-	public static final int INSERT_ID = Menu.FIRST;
+	private static final int ACTIVITY_CREATE = 0;
+	private static final int ACTIVITY_EDIT = 1;
 	
-	private int mTaskNumber = 1;
+	private static final int INSERT_ID = Menu.FIRST;
+	private static final int DELETE_ID = Menu.FIRST + 1;
+	
 	private TasksDbAdapter mDbHelper;
 	
     @Override
@@ -20,6 +29,7 @@ public class Tasker extends ListActivity {
 		mDbHelper = new TasksDbAdapter(this);
 		mDbHelper.open();
 		fillData();
+		registerForContextMenu(getListView());
     }
 
 	@Override
@@ -28,21 +38,53 @@ public class Tasker extends ListActivity {
 		menu.add(0, INSERT_ID, 0, R.string.menu_insert);
 		return result;
 	}
-	
+
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
 			case INSERT_ID:
 				createTask();
 				return true;
 		}
+		
+		return super.onMenuItemSelected(featureId, item);
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case DELETE_ID:
+				AdapterContextMenuInfo info = (AdapterContextMenuInfo)item.getMenuInfo();
+				mDbHelper.deleteTask(info.id);
+				fillData();
+				return true;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 	
-	private void createTask() {
-		String taskName = "Task " + mTaskNumber++;
-		mDbHelper.createTask(taskName, "");
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Intent i = new Intent(this, TaskEdit.class);
+		i.putExtra(TasksDbAdapter.KEY_ROWID, id);
+		startActivityForResult(i, ACTIVITY_EDIT);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
 		fillData();
+	}
+	
+	private void createTask() {
+		Intent i = new Intent(this, TaskEdit.class);
+		startActivityForResult(i, ACTIVITY_CREATE);
 	}
 
 	private void fillData() {
